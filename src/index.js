96e6e449999484ac4f3585998708b3017d175f20
BrunoSprint <brunosprintdev@outlook.com>
2023-12-30 19:30:29 -0300

    .

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Hello Word!!!")
+const http = require("http")
+
+const hostname = "localhost"
+const port = 3333
+
+const server = http.createServer((req, res)=>{
+    res.setHeader("Content-type", "text/plain")
+    res.end("HelloWord!!!")
+})
+
+server.listen(port, hostname, ()=>{
+    console.log("SERVIDOR RODANDO/ATIVO")
+})
